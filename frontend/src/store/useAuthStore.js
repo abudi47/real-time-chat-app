66a580b5caa78f60460b02459261ea0fc7d62d5a
Abudi <abduselamt47@gmail.com>
2025-06-30 15:22:06 +0300

    deployment

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,9 @@ import api from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL =
+  import.meta.env.MODE === "develepment" ? "http://localhost:5001/api" : "/"; // Replace with your API base URL
+
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
