67ccf4b17a6e548c9264737dd5f29395872cd2d7
Abudi <abduselamt47@gmail.com>
2025-07-01 07:48:44 +0300

    fix route

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,13 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDb } from "./lib/db.js";
-import { app , server} from "./lib/socket.js"
+import { app, server } from "./lib/socket.js";
 import path from "path";
+import index from "../../";
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,12 +27,12 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../frontend" , "dist" , "index.html"))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
