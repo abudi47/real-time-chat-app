b12f0e1db5ce1131b3c9bd7808d19c5f7a3a4480
abudi47 <abduselamt47@gmail.com>
2025-09-24 10:16:14 +0300

    socket

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,7 +8,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://real-time-chat-app-yp8r.vercel.app",
+    ],
+
     credentials: true,
   },
 });
