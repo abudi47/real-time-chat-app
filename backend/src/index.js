744eb66f3a808765cb8b89096038fdb0c6b0e4d0
abudi47 <abduselamt47@gmail.com>
2025-09-24 10:07:35 +0300

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://real-time-chat-app-88zz.vercel.app/",
+      "https://real-time-chat-app-yp8r.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
